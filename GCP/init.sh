4f3eea4d09ed9c7c4efbbdaadff01fcf804c8efa
Willian Braga da Silva <neverping@gmail.com>
2022-08-05 16:39:11 +0200

    Update init.sh
    Now it creates secrets and manages them.

--- GCP/init.sh
+++ GCP/init.sh
@@ -2,6 +2,14 @@
 export PROJECT_ID=$(gcloud config get-value project)
 export GCP_REGION="us-central1"
 export GCP_ZONE="$(gcloud compute zones list --filter=name=${GCP_REGION} --format="value(name)" --limit=1)"
+export SECRET_NAME="facebook-token"
 gcloud config set compute/region "${GCP_REGION}"
 gcloud config set compute/zone "${GCP_ZONE}"
-gcloud services enable secretmanager.googleapis.com container.googleapis.com cloudbuild.googleapis.com 
+gcloud services enable secretmanager.googleapis.com container.googleapis.com cloudbuild.googleapis.com
+gcloud secrets create ${SECRET_NAME} --replication-policy="user-managed" --locations=${GCP_REGION}
+echo "the-first-version" | gcloud secrets versions add ${SECRET_NAME} --data-file=-
+echo "the-second-version" | gcloud secrets versions add ${SECRET_NAME} --data-file=-
+echo "the-third-version" | gcloud secrets versions add ${SECRET_NAME} --data-file=-
+echo "the-fourth-version" | gcloud secrets versions add ${SECRET_NAME} --data-file=-
+gcloud secrets versions disable 4 --secret="${SECRET_NAME}"
+gcloud secrets versions destroy 1 --secret="${SECRET_NAME}"
