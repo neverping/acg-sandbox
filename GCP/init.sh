19755cdec4123f7b0597635fae2637dee33bcf8b
Willian Braga da Silva <neverping@gmail.com>
2022-08-05 10:54:31 +0200

    Add services to be enabled
    We will enable Secret Manager, K8s, and Cloud Build

--- GCP/init.sh
+++ GCP/init.sh
@@ -2,5 +2,6 @@
 export PROJECT_ID=$(gcloud config get-value project)
 export GCP_REGION="us-central1"
 export GCP_ZONE="$(gcloud compute zones list --filter=name=${GCP_REGION} --format="value(name)" --limit=1)"
-gcloud config set compute/region="${GCP_REGION}"
-gcloud config set compute/zone="${GCP_ZONE}"
+gcloud config set compute/region "${GCP_REGION}"
+gcloud config set compute/zone "${GCP_ZONE}"
+gcloud services enable secretmanager.googleapis.com container.googleapis.com cloudbuild.googleapis.com 
