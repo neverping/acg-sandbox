bccb48cd870a3e8f1edccc097cf302d055476a8f
Willian Braga da Silva <neverping@gmail.com>
2022-08-09 15:43:36 +0200

    NEW: Add SecretManager viewer permission
    This is needed to list secret versions.

--- GCP/02_populate_cluster.sh
+++ GCP/02_populate_cluster.sh
@@ -9,10 +9,6 @@ export YAML_BUNDLE="./echo_server_with_sidecar.yaml"
 kubectl create serviceaccount ${SECRET_MANAGER_SA_NAME} --namespace ${K8S_NAMESPACE}
 gcloud iam service-accounts create ${SECRET_MANAGER_SA_NAME} --project=${PROJECT_ID}
 
-#gcloud projects add-iam-policy-binding PROJECT_ID \
-#    --member "serviceAccount:${SECRET_MANAGER_SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com" \
-#    --role "roles/spanner.viewer"
-
 gcloud iam service-accounts add-iam-policy-binding ${SECRET_MANAGER_SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com \
     --role roles/iam.workloadIdentityUser \
     --member "serviceAccount:${PROJECT_ID}.svc.id.goog[${K8S_NAMESPACE}/${SECRET_MANAGER_SA_NAME}]"
@@ -22,12 +18,16 @@ gcloud secrets add-iam-policy-binding ${SECRET_NAME} \
     --member='serviceAccount:${SECRET_MANAGER_SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com' \
     --role='roles/secretmanager.secretAccessor'
 
+# HINT: We allow directly at the RESOURCE level because ACG doesn't allow our USER ACCOUNTS to grant permissions at the PROJECT level.
+gcloud secrets add-iam-policy-binding ${SECRET_NAME} \
+    --member='serviceAccount:${SECRET_MANAGER_SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com' \
+    --role='roles/secretmanager.viewer'
+
 kubectl annotate serviceaccount ${SECRET_MANAGER_SA_NAME} \
     --namespace ${K8S_NAMESPACE} \
     iam.gke.io/gcp-service-account=${SECRET_MANAGER_SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com
 
 # TODO: This should be a Helm.
-
 if [[ -f ${YAML_BUNDLE} ]]; then
   rm ${YAML_BUNDLE}
 fi
