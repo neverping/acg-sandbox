5653046f1945590b39efd6403ca3af4d99ea4ada
Willian Braga da Silva <neverping@gmail.com>
2022-08-09 15:48:31 +0200

    NEW: Add access builder
    This adds a small script to verify our work.

---
+++ GCP/03_access_builder.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -euo pipefail
+
+source ./00_variables.sh
+
+export EXTERNAL_IP=$(kubectl get svc ${SECRET_MANAGER_SA_NAME} -o=jsonpath='{.status.loadBalancer.ingress[0].ip})
+
+echo "Your request can be done using \"curl http://${EXTERNAL_IP}/?echo_file=/mnt/secret-manager/facebook/facebook-token.txt\" endpoint."
