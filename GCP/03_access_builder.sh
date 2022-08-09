042cdfbbfd6368a5da7db1a7fe6413c31c29636f
Willian Braga da Silva <neverping@gmail.com>
2022-08-09 15:49:26 +0200

    FIX: Closing quote

--- GCP/03_access_builder.sh
+++ GCP/03_access_builder.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 source ./00_variables.sh
 
-export EXTERNAL_IP=$(kubectl get svc ${SECRET_MANAGER_SA_NAME} -o=jsonpath='{.status.loadBalancer.ingress[0].ip})
+export EXTERNAL_IP=$(kubectl get svc ${SECRET_MANAGER_SA_NAME} -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
 echo "Your request can be done using \"curl http://${EXTERNAL_IP}/?echo_file=/mnt/secret-manager/facebook/facebook-token.txt\" endpoint."
