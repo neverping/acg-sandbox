9cc062d6234dcd89ae4ddd203fef34e55998a32c
Willian Braga da Silva <neverping@gmail.com>
2023-01-26 11:36:19 +0100

    Remove plugin install
    It's being done on Ansible.

--- GCP/02_cluster_setup.sh
+++ GCP/02_cluster_setup.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 source ./00_variables.sh
 
 # We need the new GKE auth plugin installed on clusters 1.21 and newer.
-gcloud components install gke-gcloud-auth-plugin
+#gcloud components install gke-gcloud-auth-plugin
 
 
 # Check if the cluster is up before attempting the next steps. Otherwise break this script from executing
